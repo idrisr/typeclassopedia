c8c79594196d8a6a4a8ab4afa3f0f7f1f0bf6989
Idris Raja <idris.raja@gmail.com>
2023-02-04 16:46:38 -0600

    moar fun functors and applicative

--- 01functor.hs
+++ 01functor.hs
@@ -1,19 +1,50 @@
+-- 1 Implement Functor instances for Either e and ((->) e).
+newtype Eether a b = Eether (Either a b)
+instance Functor (Eether a) where
+    fmap f (Eether (Right b)) = Eether $ Right $ f b
+    fmap _ (Eether (Left a))  = Eether $ Left a
 
-newtype Tuple a b = Tuple (a, b)
-newtype Func a b = Func ((->) a b)
-newtype EZ a b = EZ (Either a b)
-
-instance Functor (Tuple a) where
-    fmap f (Tuple (a, b)) = Tuple (a, f b)
-
+newtype Func a b = Func (a -> b)
 instance Functor (Func a) where
-    fmap g (Func f) = Func (g . f)
+    -- f :: a->b
+    -- g :: e->a
+    fmap f (Func g) = Func $ f . g
 
-instance Functor (EZ a) where
-    fmap f (EZ (Left a)) = EZ (Left a)
-    fmap f (EZ (Right b)) = EZ (Right (f b))
+-- 2 Implement Functor instances for ((,) e) and for Pair, defined as
+-- Explain their similarities and differences.
+-- Pair takes one type parameter
+-- Tople takes two type parameters
+data Pair a = Pair a a
+instance Functor Pair where
+    fmap f (Pair a b) = Pair (f a) $ f b
 
--- A good exercise is to implement Functor instances for
--- Either e
--- ((,) e)
--- ((->e)f
+newtype Tople a b = Tople (a, b)
+instance Functor (Tople a) where
+    fmap f (Tople (x, y)) = Tople (x, f y)
+
+-- 3. Implement a Functor instance for the type ITree, defined as
+data ITree a = Leaf (Int -> a)
+             | Node [ITree a]
+
+instance Functor ITree where
+    fmap f (Leaf g)  = Leaf $ f . g
+    fmap f (Node xs) = Node $ fmap g xs
+        where g = fmap f
+
+-- 4. Give an example of a type of kind * -> * which cannot be made an instance
+-- of Functor (without using undefined).
+newtype Funk a b = Funk (b -> a)
+
+-- instance Functor (Funk z) where
+--  fmap f (Funk g)
+    -- f :: a -> b
+    -- g :: z -> b
+    -- f and g cant be composed for fmap
+
+-- 5. Is this statement true or false?
+-- The composition of two Functors is also a Functor.
+-- If false, give a counterexample; if true, prove it by exhibiting some
+-- appropriate Haskell code.
+--
+-- f a
+-- G a
--- 02applicative.hs
+++ 02applicative.hs
@@ -8,17 +8,32 @@ instance Functor MMaybe where
     fmap f (MMaybe Nothing)  = MMaybe Nothing
 
 instance Applicative MMaybe where
-    pure a = MMaybe (Just a)
-    _                 <*> (MMaybe Nothing)  = MMaybe Nothing
-    (MMaybe Nothing)  <*> _                 = MMaybe Nothing
-    (MMaybe (Just f)) <*> (MMaybe (Just a)) = MMaybe (Just $ f a)
+    pure a                              = MMaybe $ Just a
+    _               <*> MMaybe Nothing  = MMaybe Nothing
+    MMaybe Nothing  <*> _               = MMaybe Nothing
+    MMaybe (Just f) <*> MMaybe (Just a) = MMaybe $ Just $ f a
 
-newtype ZipList a = ZipList { getZipList :: [a] }
+newtype ZipList a = ZipList { getZipList :: [a] } deriving Show
 
 instance Functor ZipList where
-    fmap f (ZipList []) = ZipList []
+    fmap f (ZipList [])     = ZipList []
     fmap f (ZipList (x:xs)) = ZipList $ f x:fmap f xs
 
 instance Applicative ZipList where
     pure a = ZipList [a]
-    (ZipList gs) <*> (ZipList xs) = ZipList $ zipWith ($) gs xs
+    ZipList gs <*> ZipList xs = ZipList $ zipWith ($) gs xs
+
+a = ZipList [(+1), (+2), (*8)]
+b = ZipList [4, 5, 6]
+c = a <*> b
+
+{- HLINT ignore "Use foldr" -}
+sequenceAL :: Applicative f => [f a] -> f [a]
+sequenceAL []     = pure []
+sequenceAL (x:xs) = (:) <$> x <*> sequenceAL xs
+-- (:)       :: a -> [a] -> [a]
+-- x         :: f a
+-- <$> (:)   :: f a -> f [a] -> f [a]
+-- <$> (:) x :: f [a] -> f [a]
+-- <*>       ::
+-- sequenceAL xs
